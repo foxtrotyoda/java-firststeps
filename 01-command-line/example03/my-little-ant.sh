740d31b1bb2d7bcc0db27c80eb862f00516d8451
Foxtrot Yoda <foxtrotyoda@8ig.uk>
2017-11-09 19:32:57 +0000

    apage

--- 01-command-line/example03/my-little-ant.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-JAVAON=`command -v java`
-
-if [ -n "$JAVAON" ]; then
-    java -jar mylittleant.jar $@ # last $@ passes all from bash to java class
-else
-    echo "Error: Java not installed!"
-    exit 1
-fi
